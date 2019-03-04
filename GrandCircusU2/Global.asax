36036dca36cfafa5e28f0c03b0b986ee780cafd2
EvanSimon <evansimon07@gmail.com>
2019-03-04 08:53:04 -0500

    e

---
+++ GrandCircusU2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GrandCircusU2.MvcApplication" Language="C#" %>
---
+++ GrandCircusU2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GrandCircusU2.Startup))]
+namespace GrandCircusU2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
